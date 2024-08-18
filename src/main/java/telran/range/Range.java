a314fbd2b9621f8976eca97d5262321d3cf8af58
DaniilJustCode <gromdan90@gmail.com>
2024-08-18 11:26:00 +0300

    Commit, bu this is not my code, cause i don't understand, how to do method hasNext

--- src/main/java/telran/range/Range.java
+++ src/main/java/telran/range/Range.java
@@ -1,21 +1,29 @@
 package telran.range;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
-public class Range {
+public class Range implements Iterable<Integer>{
     private static final String ERROR_MESSAGE = "max less or equal min";
     private int min;
     private int max;
+    private Predicate<Integer> predicate = n -> true;
     private Range(int min, int max) {
         this.min = min;
-        this.max = max;
+        this.max = max; 
     }
     public static Range getRange(int min, int max) {
         if (max <= min) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return new Range(min, max);
+    }
+    void setPredicate(Predicate<Integer> predicate) {
+        this.predicate = predicate;
     }
     public void checkNumber(int number) throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
         if(number > max) {
@@ -24,5 +32,39 @@ public class Range {
         if (number < min) {
             throw new OutOfRangeMinValueException(min, number);
         }
+    }
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangeIterator();
+    }
+    private class RangeIterator implements Iterator<Integer> {
+        int current = min - 1;
+        @Override
+        public boolean hasNext() {
+           
+            return current <= max;
+        }
+        public RangeIterator() {
+            setNextCurrent();
+        }
+
+        @Override
+        public Integer next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int toBeIterated = current;
+            setNextCurrent();
+            return toBeIterated;
+        }
+
+        private void setNextCurrent() {
+            current++;
+            while(current <= max && !predicate.test(current)) {
+                current++;
+            }
+            
+        }
+        
     }
 }
--- src/test/java/telran/range/RangeTest.java
+++ src/test/java/telran/range/RangeTest.java
@@ -2,6 +2,9 @@ package telran.range;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.Test;
 
 import telran.range.exceptions.OutOfRangeMaxValueException;
@@ -11,10 +14,12 @@ public class RangeTest {
     private static final int MIN = 50;
     private static final int MAX = 100;
     Range range = Range.getRange(MIN, MAX);
+
     @Test
     void wrongRangeCtreatingTest() {
         assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN) );
     }
+
     @Test
     void rightNumberTest() throws Exception {
         range.checkNumber(55);
@@ -22,5 +27,36 @@ public class RangeTest {
     void wrongNumberTest() throws Exception {
         assertThrowsExactly(OutOfRangeMaxValueException.class, () -> range.checkNumber(MAX + 1));
         assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
+    }
+
+     @Test
+    void iteratorTest() {
+        Range range = Range.getRange(1, 3);
+        runTest(new Integer[] {1,2,3}, range);
+    }
+    @Test
+    void iteratorPredicateMatchTest() {
+        Range range = Range.getRange(1, 3);
+        range.setPredicate(n -> n % 2 == 0); 
+        runTest(new Integer[]{2}, range);
+        range.setPredicate(n -> n % 2 != 0); 
+        runTest(new Integer[]{1, 3}, range);
+
+    }
+    @Test
+    void iteratorPredicateNoMatchTest() {
+        range.setPredicate(n -> n > MAX);
+        runTest(new Integer[0], range);
+
+    }
+    private void runTest(Integer[] expected, Range range) {
+        Integer [] actual = new Integer[expected.length];
+        Iterator<Integer> it = range.iterator();
+        int index = 0;
+        while(it.hasNext()) {
+            actual[index++] = it.next();
+        }
+        assertArrayEquals(expected, actual);
+        assertThrowsExactly(NoSuchElementException.class, it::next);
     }
 }
